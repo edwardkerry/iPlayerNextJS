af2ec311bf1cbada0e067c3749ddfb0498d2a178
Ed Kerry <edwardkerry@notonthehighstreet.com>
2017-08-18 11:15:22 +0100

    Add initial pages

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Header = () => (
+  <div>
+    <h1>BBC iPlayer programme search</h1>
+    <Link href="/">
+        <a> BBC A - Z Programme Search</a>
+    </Link>
+  </div>
+)
+
+  export default Header
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Header from '../components/Header'
+
+export default () => (
+  <div>
+  <Header />
+    <h1>iPlayer Programme Search</h1>
+  </div>
+)
